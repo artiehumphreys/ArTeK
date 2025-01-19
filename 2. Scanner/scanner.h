baf49ac14ad02a53311856e99580f823daa23e68
artiehumphreys <artieh2003@gmail.com>
2025-01-19 18:50:58 -0500

    Added Makefile

--- 2. Scanner/scanner.h
+++ 2. Scanner/scanner.h
@@ -1,6 +1,8 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+// Still don't know why this definition is necessary, but it is required to inherit from yyFlexLexer.
+// https://stackoverflow.com/questions/40663527/how-to-inherit-from-yyflexlexer
 #if !defined(yyFlexLexerOnce)
 #include <FlexLexer.h>
 #endif
@@ -10,6 +12,7 @@
 
 class Scanner : public yyFlexLexer {
 public:
+    // yyFlexLexer object requires input and output streams
     Scanner(std::istream* in = nullptr, std::ostream* out = nullptr)
         : yyFlexLexer(in, out), currentToken() {}
 
